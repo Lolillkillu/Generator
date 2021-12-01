942809bb9ac054ad282d95d3f25964029ed7b1be
Lolillkillu <63799243+Lolillkillu@users.noreply.github.com>
2021-12-01 18:19:32 +0100

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -2,7 +2,7 @@ import random
 import binascii 
 import hashlib
 
-
+#a = "ABCD" - Test 
 a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 b = a.lower()
 c = "1234567890!@#$%^&*"
@@ -13,7 +13,7 @@ x = int(input("min: "))
 y = int(input("max: "))
 
 z = str(input("NTLM: "))
-#z = "EB4FF39B74B0CBCE20A4F62DBD1E3585"
+#z = "EB4FF39B74B0CBCE20A4F62DBD1E3585" - Test 
  
 
 def randstr(length):
@@ -32,10 +32,10 @@ check = True
 while(check):
 	temp = randstr(random.randrange(x, y+1))
 	print(temp)
-	#temp = "abcd"
+	#temp = "abcd" - Test 
 	ntlm = binascii.hexlify(hashlib.new('md4', temp.encode('utf-16le')).digest())
 	print(ntlm)
-	#print("b'"+z.lower()+"'")
+	#print("b'"+z.lower()+"'") - Test 
 
 	if(str(ntlm.lower()) == ( "b'"+z.lower()+"'")):
 		check = False
